3988e2ecf85e9848f99c8e820400ade73e6992a8
Sathwik <sathwiknayak199@gmail.com>
2023-12-06 01:46:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { accordionData } from "./utils/content";
 import Main from "./Main";
 
 function App() {
   return (
     <div>
-      <Main />
+      <div className="accordion">
+        {accordionData.map(({ title, content }) => (
+          <Main key={title} title={title} content={content} />
+        ))}
+      </div>
     </div>
   );
 }
