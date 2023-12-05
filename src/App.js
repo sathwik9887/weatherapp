a146b827d57ec6bbcb32e4e21c6339b3c1563058
Sathwik <sathwiknayak199@gmail.com>
2023-12-06 02:12:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { accordionData } from "./utils/content";
 import Main from "./Main";
 
 function App() {
   return (
     <div>
-      <div className="accordion">
-        {accordionData.map(({ title, content }) => (
-          <Main key={title} title={title} content={content} />
-        ))}
-      </div>
+      <Main />
     </div>
   );
 }
--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,46 @@
 import React, { useState } from "react";
 
-function Main({ title, content }) {
-  const [isActive, setIsActive] = useState(false);
+function Main() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const testimonials = [
+    {
+      quote: "This is the best product",
+      author: "John",
+    },
+    {
+      quote: "Another great testimonial",
+      author: "Doe",
+    },
+    {
+      quote: "Yet another positive review",
+      author: "Smith",
+    },
+  ];
+
+  const handlePrevClick = () => {
+    setCurrentIndex(
+      (currentIndex + testimonials.length - 1) % testimonials.length
+    );
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((currentIndex + 1) % testimonials.length);
+  };
 
   return (
-    <>
-      <section className="accordion-card">
-        <div className="header" onClick={() => setIsActive(!isActive)}>
-          <div>{title}</div>
-          <p className="icon">{isActive ? "-" : "+"}</p>
-        </div>
-        <div className="content">
-          {isActive && <p className="card-info">{content}</p>}
-        </div>
-      </section>
-    </>
+    <div className="testimonials">
+      <div className="testimonials-quote">
+        {testimonials[currentIndex].quote}
+      </div>
+      <div className="testimonials-author">
+        {testimonials[currentIndex].author}
+      </div>
+      <div className="nav">
+        <button onClick={handlePrevClick}>Prev</button>
+        <button onClick={handleNextClick}>Next</button>
+      </div>
+    </div>
   );
 }
 
